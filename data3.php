a7d851dcc5c1be852c587c2d2cb30c6212c7f511
StrekanovAlexander <strekanov.alexander@gmail.com>
2021-01-06 10:21:04 +0200

    done: base functional

---
+++ data3.php
@@ -0,0 +1,59 @@
+<?php
+    require 'db.php';
+ 
+    $res = mysqli_query($db, "SELECT * FROM titles ORDER BY id DESC LIMIT 5");
+ 
+    $titles = [];
+    if ($res){
+        while($row = mysqli_fetch_assoc($res)){
+            $titles[] = $row;
+        }   
+    }
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset = "utf-8" />
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+<body>
+    <h3>Ajax-loading by button click</h3>
+    <div id="titles">
+        <?php foreach ($titles as $title): ?>
+            <p><?php echo $title['title']; ?></p>
+        <?php endforeach ?>
+    </div>
+    <button id="more">More...</button>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                var inProgress = false;
+                var startFrom = 5;
+ 
+                $('#more').click(function() {
+                if (!inProgress) {
+                    $.ajax({
+                        url: 'ajax.php',
+                        method: 'POST',
+                        data: {
+                        "start" : startFrom
+                    },
+                    beforeSend: function() {
+                        inProgress = true;
+                    }
+                }).done(function(data){
+                    data = jQuery.parseJSON(data); 
+                    if (data.length > 0){
+                        $.each(data, function(index, data){
+                            $("#titles").append("<p>" + data.title + "</p>");
+                        });
+                        inProgress = false;
+                        startFrom += 5;
+                    }
+                });
+            }
+        });
+    });
+    </script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,14 +2,34 @@
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App();
+$app = new \Slim\App([
+  'settings' => [
+    'displayErrorDetails' => true
+  ]
+]);
 
-$app->get('/', function(){
-  echo 'page';
-});
+$c = $app->getContainer();
 
-$app->get('/db', function(){
-  echo 'db';
-});
+$c['view'] = function($c) {
+  
+  $view = new \Slim\Views\Twig(__DIR__ . '/views', [
+    'cashe' => false
+  ]);
+  
+  $view->addExtension(new \Slim\Views\TwigExtension(
+    $c->router,
+    $c->request->getUri()
+  ));
+
+  return $view;
+}; 
+
+$app->get('/', function($req, $res) {
+  return $this->view->render($res, 'index.twig');
+})->setName('index');
+
+$app->get('/data', function($req, $res) {
+  return $this->view->render($res, 'data.twig');
+})->setName('data');
 
 $app->run();
